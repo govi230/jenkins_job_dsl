ab8198dab4ef214a1008b08c85908f8c10354b60
root <root@localhost.localdomain>
2021-07-30 00:11:32 -0400

    Web Deploy

--- test.groovy
+++ test.groovy
@@ -1,8 +1,16 @@
-import  config
-
 job("WebDeploy"){
+    // 'github_repo' variable contains username and reponame
+    // Like   '--username--/--reponame--'
+    github_repo = "govi230/okd_training"
+    
+    // Token Value (With the help of this Token Job Can be run remotely with URL)
+    job_token = "redhat"
+    
+    // 'job_description' contains description about JOB
+    job_description = "Web Deployment"
+
     // Provide Authentication Token
-    authenticationToken(config.job_token)
+    authenticationToken(job_token)
     // Job Description 
     description(job_description)
     // Agent Label where this job will be run
