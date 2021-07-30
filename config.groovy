5aa00b94af01acd55663943d01b3ca569e353a29
root <root@localhost.localdomain>
2021-07-29 23:41:12 -0400

    Web Deploy

---
+++ config.groovy
@@ -0,0 +1,9 @@
+// 'github_repo' variable contains username and reponame
+// Like   '--username--/--reponame--'
+github_repo = "govi230/okd_training"
+
+// Token Value (With the help of this Token Job Can be run remotely with URL)
+job_token = "redhat"
+
+// 'job_description' contains description about JOB
+job_description = "Web Deployment"
