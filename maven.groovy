afb5c621635eeef8220bcb8a818126a92848fb2c
root <root@localhost.localdomain>
2021-07-31 00:24:15 -0400

    Testing

---
+++ maven.groovy
@@ -0,0 +1,48 @@
+job("Maven JOb"){
+    // 'github_repo' variable contains username and reponame
+    // Like   '--username--/--reponame--'
+    github_repo = "govi230/simple-java-maven-app"
+    // Token Value (With the help of this Token Job Can be run remotely with URL)
+    job_token = "redhat"
+    // 'job_description' contains description about JOB
+    job_description = "Maven Build Process"
+    // 'display_name' contain Name which will we display on Jenkins Jos Dashboard
+    display_name = "Sample Maven Job App"
+    // 'scm_target_dir' contains relative directory path from Job Workspace 
+    // where GitHub repository will be stored
+    scm_target_dir = "maven_app"
+    // Maven Installation Name
+    maven_nstallation = "maven-3.8.1"
+
+
+    // Provide Authentication Token
+    authenticationToken(job_token)
+    // Job Description 
+    description(job_description)
+    // Display Name
+    displayName(display_name)
+    // Agent Label where this job will be run
+    label("sshAgent")
+    scm{
+        git{
+            remote{
+                branch("master")
+                github(
+                    ownerAndProject = github_repo,
+                    protocol = "https",
+                    host = "github.com"
+                )
+            }
+            extensions{
+                relativeTargetDirectory(scm_target_dir)
+            }
+        }
+    }
+    steps{
+        maven{
+            mavenInstallation(maven_nstallation)
+            goals("package")
+        }
+    }
+
+}
